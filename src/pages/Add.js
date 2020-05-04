bc4078bb8e947b0a2e93b948d36e499b739653b1
Artem Deikun <artem.deikun@zorallabs.com>
2020-05-04 12:24:39 +0300

    Add form submit data

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,5 +1,7 @@
-import React, {useReducer} from "react";
+import React, {useReducer, useState} from "react";
 import PropTypes from "prop-types";
+
+import config from '../config';
 
 Add.propTypes = {};
 
@@ -10,14 +12,46 @@ const initValue = {
   userDob: '',
 };
 
-const reducer = (state, {field, value}) => ({ ...state, [field]: value });
+const reducer = (state, {field, value, action}) => {
+  if (action === 'RESET') {
+    return initValue;
+  }
+  return ({ ...state, [field]: value })
+};
 
 function Add (props) {
+
+  const [formSubmitStatus, setFormSubmitStatus] = useState({
+    status: null
+  });
+  const [isLoaded, setIsLoaded] = useState(false);
+
   const [state, dispatch] = useReducer(reducer, initValue);
   const {userDob, userEmail, userName, userPhone} = state;
 
   const handleSubmit = (event) => {
-    console.log(state);
+    setIsLoaded(true);
+    fetch(config.backend, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(state)
+    }).then(res => res.json()).then((result) => {
+      setFormSubmitStatus({
+        status: 'success'
+      })
+      console.log(result, 'API');
+      dispatch({
+          action: 'RESET'
+      })
+    }, (error) => {
+      setFormSubmitStatus({
+        status: 'error',
+        errorDetails: error
+      })
+    }).finally(()=>{
+      setIsLoaded(false);
+
+    });
     event.preventDefault();
   };
 
@@ -26,23 +60,30 @@ function Add (props) {
       value: event.target.value
   })
 
+  const showFormStatus = () => {
+    if (!formSubmitStatus.status) { return null; }
+    return <section> {formSubmitStatus.status} </section>
+  }
+
   return (
     <form onSubmit={handleSubmit}>
+        {isLoaded && 'Loading'}
+        {showFormStatus()}
         <label>
           Name:
-          <input type="text" name="userName" value={userName} onChange={handleInputChange} />
+          <input type="text" required name="userName" value={userName} onChange={handleInputChange} />
         </label>
         <label>
           Email:
-          <input type="email" name="userEmail" value={userEmail} onChange={handleInputChange} />
+          <input type="email" required name="userEmail" value={userEmail} onChange={handleInputChange} />
         </label>
         <label>
           Phone:
-          <input type="tel" name="userPhone" value={userPhone} onChange={handleInputChange} />
+          <input type="tel" required name="userPhone" value={userPhone} onChange={handleInputChange} />
         </label>
         <label>
           DoB:
-          <input type="date" name="userDob" value={userDob} onChange={handleInputChange} />
+          <input type="date" required name="userDob" value={userDob} onChange={handleInputChange} />
         </label>
         <input type="submit" value="Submit" />
     </form>
