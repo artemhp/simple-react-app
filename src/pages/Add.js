96073e0773bc5cc42db0d929fd6f6ab40347c9a0
Artem Deikun <artem.deikun@zorallabs.com>
2020-05-04 11:19:00 +0300

    Routing

---
+++ src/pages/Add.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Add.propTypes = {
+
+};
+
+function Add(props) {
+    return (
+        <div>
+            Add
+        </div>
+    );
+}
+
+export default Add;
