49a57034c2fb7d670257379ee5f6278975fd0042
Artem Deikun <artem.deikun@zorallabs.com>
2020-09-19 14:43:23 +0300

    Init test

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
           <div className="navbar-menu">
             <div className="navbar-start">
               <Link className="navbar-item" to="/">
-                Home2
+                Home
               </Link>
               <Link className="navbar-item" to="/add">
                 Add
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Check layout:', () => {
+  let container = null;
+  beforeEach(() => {
+    ({ container } = render(<App />));
+  });
+  test('Renders footer', () => expect(container.querySelector('footer')).toBeInTheDocument());
+  test('Renders main', () => expect(container.querySelector('main')).toBeInTheDocument());
+  test('Renders navigation', () => expect(container.querySelector('nav')).toBeInTheDocument());
+
 });
